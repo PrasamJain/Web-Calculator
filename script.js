ecdaea687f2a18b1dd42b94f568c6fd95a2d7904
pj1807 <prasamjain294@gmail.com>
2022-06-15 14:14:35 +0530

    Script Js File

---
+++ script.js
@@ -0,0 +1,16 @@
+function clearScreen()
+{
+    document.getElementById("result").value = "";
+}
+
+function display(value)
+{
+    document.getElementById("result").value += value;
+}
+
+function calculate()
+{
+    var a = document.getElementById("result").value;
+    var b = eval(a);
+    document.getElementById("result").value = b;
+}
